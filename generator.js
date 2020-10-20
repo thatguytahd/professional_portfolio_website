d34573ee5c5a1c51b0260c90ab5e5b2a1376eceb
Todd Baker <todd.baker90@gmail.com>
2020-10-19 20:30:17 -0400

    Implemented random quote generator into project and tested successfully.

--- generator.js
+++ generator.js
@@ -1,5 +1,6 @@
-let facts = [
-    "I gradueated with Bachelors of Science in Criminal Justic and spent the first 3 years out of college working at a state prison.",
+// Array that houses random facts
+const facts = [
+    "I graduated with a Bachelors of Science in Criminal Justice and spent the first 3 years out of college working at a state prison.",
     "I am married and have two cats, Sam and Milo.",
     "I have visited the desert during a snow storm.",
     "I work for a restaurant technology company called QSR Automations."
@@ -10,6 +11,8 @@ function randomNumber () {
 }
 
 function generateFact () {
-    let randomFact = facts[randomNumber]
-    document.getElementById("bannerInfo").innerHTML = randomFact;
+    let randomFact = facts[randomNumber()]
+    return document.getElementById("bannerInfo").innerHTML = randomFact
 }
+
+generateFact()
