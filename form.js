c36deb65a9b27e88e2b1333f1aff82d7b7e333f2
Todd Baker <todd.baker90@gmail.com>
2020-11-02 16:23:47 -0500

    Added in email validation js functionality to the contact form

---
+++ form.js
@@ -0,0 +1,18 @@
+function emailIsValid (email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+  }
+
+function emailCheck() {
+    let x = document.getElementById('email').value
+
+    let validResult = emailIsValid(x)
+
+    if (validResult == false) {
+        return alert('Enter valid email. Example: JohnSmith@test.com')
+    } else {
+        alert('Contact Information Submitted!')
+        document.getElementById('formId').reset()
+    }
+}
+
+document.getElementById('submitBtn').addEventListener('click', emailCheck)
